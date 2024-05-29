c7e7c85d553e5a4d7baea0d03dc9c0fd0fb08e3f
Guilherme Roberto dos Passos <115484995+gui1ps@users.noreply.github.com>
2024-05-29 07:44:27 -0300

    Commit 4

--- semTextosDocumentos.py
+++ semTextosDocumentos.py
@@ -1,14 +1,18 @@
-import pandas as pd
 from dataframeClass import DataFrameReview
 from wordCloudGen import WordCloudGenerator
 from SimilarityScatterPlotGenerator import SimilarityScatterPlotGen
+from BarPlot import BarPlotGen
 
 filePath = './chatgpt_reviews.csv'
+pdfpath='./Interactive_Data_Visualization_ Foundations_Techniques_and_Applications_Second_Edit.pdf'
 myDataframe = DataFrameReview(filePath)
+
 periodo = ('2024-05-20','2024-06-20')
 dataFrame=myDataframe.getDataFrame(coluna=['userName','content','at','lang'],periodo=periodo,lang=True)
 wordCloudGen=WordCloudGenerator(dataFrame)
 similarityScatterPlotGen=SimilarityScatterPlotGen(dataFrame,'content','lang','userName')
+bpg=BarPlotGen(pdfpath,'portuguese')
 
-similarityScatterPlotGen.generate_similarity_scatter_plot()
 wordCloudGen.getWordCloud('content','lang',mix=False,langs=['english'])
+bpg.getBarPlot()
+similarityScatterPlotGen.generate_similarity_scatter_plot()
